d8ea8e09af977b26de1c9c9b7569572169aa8c12
mailliw17 <williamwirahardi@gmail.com>
2023-05-27 10:43:13 +0700

    condition for header and footer for seller buyer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-e-commz';
+  urlSplit : String
+
+  constructor(public route: ActivatedRoute) { }
+
+  ngOnInit() {
+    var a = window.location.pathname.split('/');
+    this.urlSplit =  a[1]
+    console.log(this.urlSplit);
+  }
 }
---
+++ src/app/pages/seller/s-dashboard-page/s-dashboard-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-s-dashboard-page',
+  templateUrl: './s-dashboard-page.component.html',
+  styleUrls: ['./s-dashboard-page.component.scss']
+})
+export class SDashboardPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
